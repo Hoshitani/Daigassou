52aa29cbc28ac0705e46eb3217558f47ccc42907
星谷望月 <326240318@qq.com>
2026-09-17 12:38:32 +0800

    增加一个多音测试

--- Daigassou/Forms/AboutForm.Designer.cs
+++ Daigassou/Forms/AboutForm.Designer.cs
@@ -28,109 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblVersion = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // lblVersion
-            // 
-            this.lblVersion.AutoSize = true;
-            this.lblVersion.BackColor = System.Drawing.Color.Transparent;
-            this.lblVersion.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lblVersion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.lblVersion.Location = new System.Drawing.Point(204, 371);
-            this.lblVersion.Name = "lblVersion";
-            this.lblVersion.Size = new System.Drawing.Size(94, 38);
-            this.lblVersion.TabIndex = 1;
-            this.lblVersion.Text = "Ver 1.1.0.54\r\n\r\n";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label2.Location = new System.Drawing.Point(239, 98);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(236, 102);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "您同意使用本软件产品风险由您自行承担\r\n包括但不限于电脑死机，封号，硬盘爆炸等\r\n为避免滥用，您同意使用本程序进行的活动\r\n不会侵犯第三方的权利。\r\n当发生滥用时" +
+			this.lblVersion = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// lblVersion
+			// 
+			this.lblVersion.AutoSize = true;
+			this.lblVersion.BackColor = System.Drawing.Color.Transparent;
+			this.lblVersion.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.lblVersion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+			this.lblVersion.Location = new System.Drawing.Point(204, 371);
+			this.lblVersion.Name = "lblVersion";
+			this.lblVersion.Size = new System.Drawing.Size(137, 56);
+			this.lblVersion.TabIndex = 1;
+			this.lblVersion.Text = "Ver 1.1.0.54\r\n\r\n";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.label2.Location = new System.Drawing.Point(239, 98);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(352, 144);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "您同意使用本软件产品风险由您自行承担\r\n包括但不限于电脑死机，封号，硬盘爆炸等\r\n为避免滥用，您同意使用本程序进行的活动\r\n不会侵犯第三方的权利。\r\n当发生滥用时" +
     "，虽然作者什么也做不了\r\n但是作者保留谴责您的权利。\r\n";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(75)))), ((int)(((byte)(107)))));
-            this.label3.Location = new System.Drawing.Point(239, 215);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(164, 102);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "程序：黑尾白猫@神意之地\r\n助手：酒酿和歌子@神意之地\r\n\r\n发布：blog.ffxiv.cat\r\nBug反馈裙：720145203\r\n制谱交流裙：3378458" +
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.BackColor = System.Drawing.Color.Transparent;
+			this.label3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(75)))), ((int)(((byte)(107)))));
+			this.label3.Location = new System.Drawing.Point(239, 215);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(247, 150);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "程序：黑尾白猫@神意之地\r\n助手：酒酿和歌子@神意之地\r\n\r\n发布：blog.ffxiv.cat\r\nBug反馈裙：720145203\r\n制谱交流裙：3378458" +
     "18\r\n";
-            // 
-            // label1
-            // 
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(415, 241);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 51);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "              ";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.label4.Location = new System.Drawing.Point(239, 250);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(140, 17);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "两只猫娘是不是很可爱！";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label5.Location = new System.Drawing.Point(80, 313);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(104, 34);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "时隔两年多才更新\r\n~感谢支持~";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // AboutForm
-            // 
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.BackColor = System.Drawing.Color.White;
-            this.BackgroundImage = global::Daigassou.Properties.Resources.about2;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(490, 362);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.lblVersion);
-            this.Controls.Add(this.label1);
-            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "AboutForm";
-            this.Padding = new System.Windows.Forms.Padding(0, 29, 0, 0);
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
-            this.Text = "关于[大合奏！]";
-            this.TopMost = true;
-            this.Load += new System.EventHandler(this.AboutForm_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Location = new System.Drawing.Point(415, 241);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(54, 51);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "              ";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.BackColor = System.Drawing.Color.Transparent;
+			this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+			this.label4.Location = new System.Drawing.Point(239, 250);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(208, 24);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "两只猫娘是不是很可爱！";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.label5.Location = new System.Drawing.Point(80, 313);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(154, 48);
+			this.label5.TabIndex = 6;
+			this.label5.Text = "时隔两年多才更新\r\n~感谢支持~";
+			this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// AboutForm
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.BackColor = System.Drawing.Color.White;
+			this.BackgroundImage = global::Daigassou.Properties.Resources.about2;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.ClientSize = new System.Drawing.Size(490, 362);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.lblVersion);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "AboutForm";
+			this.Padding = new System.Windows.Forms.Padding(0, 29, 0, 0);
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.Text = "关于[大合奏！]";
+			this.TopMost = true;
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AboutForm_FormClosed);
+			this.Load += new System.EventHandler(this.AboutForm_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
--- Daigassou/Forms/AboutForm.cs
+++ Daigassou/Forms/AboutForm.cs
@@ -10,6 +10,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Daigassou.Input_Midi;
 using Sunny.UI;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
 
 namespace Daigassou
 {
@@ -21,12 +24,50 @@ namespace Daigassou
             
             InitializeComponent();
         }
-
-        private void AboutForm_Load(object sender, EventArgs e)
+		CancellationTokenSource cts;
+		private async void AboutForm_Load(object sender, EventArgs e)
         {
             lblVersion.Text = "Ver " + Assembly.GetExecutingAssembly().GetName().Version;
-        }
+			//cts = new CancellationTokenSource();
+			//Do(cts.Token);
+		}		
+		//async Task Do(CancellationToken token)
+		//{
+		//	int i = 80;
+		//	while (!token.IsCancellationRequested&&i>=0)
+		//	{
+		//		try
+		//		{
+		//			DaigassouDX.Controller.ProcessKeyController.GetInstance().PressKeyBoardByPitch(48);
+		//			await Task.Delay(i, token);
+		//			DaigassouDX.Controller.ProcessKeyController.GetInstance().PressKeyBoardByPitch(55);
+		//			await Task.Delay(i, token);
+		//			DaigassouDX.Controller.ProcessKeyController.GetInstance().PressKeyBoardByPitch(60);
+		//			await Task.Delay(i, token);
+		//			DaigassouDX.Controller.ProcessKeyController.GetInstance().PressKeyBoardByPitch(67);
+		//			await Task.Delay(i, token);
+		//			DaigassouDX.Controller.ProcessKeyController.GetInstance().PressKeyBoardByPitch(72);
+		//			DaigassouDX.Controller.ProcessKeyController.GetInstance().ReleaseKeyBoardByPitch(48);
+		//			DaigassouDX.Controller.ProcessKeyController.GetInstance().ReleaseKeyBoardByPitch(55);
+		//			DaigassouDX.Controller.ProcessKeyController.GetInstance().ReleaseKeyBoardByPitch(67);
+		//			DaigassouDX.Controller.ProcessKeyController.GetInstance().ReleaseKeyBoardByPitch(60);
+		//			DaigassouDX.Controller.ProcessKeyController.GetInstance().ReleaseKeyBoardByPitch(72);
+		//			await Task.Delay(500, token);
+		//			Debug.WriteLine($"在{i}ms下的琶音");
+		//			i -= 2;
+		//			await Task.Delay(500, token);
 
+		//		}
+		//		catch (Exception)
+		//		{
+		//			break;
+		//		}
+		//	}
 
-    }
+		//}
+		private void AboutForm_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			//cts.Cancel();
+		}
+	}
 }
